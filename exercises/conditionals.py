281fd1989061b5623b4c3b00d8966d41e444c5fa
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-13 14:56:20 -0300

    add conditional file

---
+++ exercises/conditionals.py
@@ -0,0 +1,14 @@
+def check_if_word_has_4_or_more_letters(word):
+    return
+
+
+def check_what_number_is_greater(first_number, second_number):
+    return
+
+
+def check_if_number_is_odd_or_even(number):
+    return
+
+
+def check_if_element_exists_in_list(element, input_list):
+    return
