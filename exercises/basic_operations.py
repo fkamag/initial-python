b34c0405c39b7dc1c3ee1139e8c4f55208cf3c49
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-13 14:55:53 -0300

    add basic operations file

---
+++ exercises/basic_operations.py
@@ -0,0 +1,26 @@
+def basic_sum(first_number, second_number):
+    return
+
+
+def basic_difference(first_number, second_number):
+    return
+
+
+def basic_product(first_number, second_number):
+    return
+
+
+def basic_division(first_number, second_number):
+    return
+
+
+def basic_integer_division(first_number, second_number):
+    return
+
+
+def basic_remainder(first_number, second_number):
+    return
+
+
+def basic_potentiation(first_number, second_number):
+    return
