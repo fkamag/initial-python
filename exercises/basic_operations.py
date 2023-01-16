ee0a99c025aa252d2ad9fc02408a588d6a11c6f8
Vitor Buxbaum Orlandi <vitor.buxbaum@gmail.com>
2023-01-16 10:17:30 -0300

    Adiciona dica sobre construção de funções

--- exercises/basic_operations.py
+++ exercises/basic_operations.py
@@ -1,3 +1,12 @@
+# Boas-vindas!
+# Para te facilitar, olha só como funcionam as funções em Python:
+#
+# Obs: 'def' é a palavra usada para definir uma nova função
+#
+# def nome_da_funcao(primeiro_parametro, segundo_parametro):
+#     variavel = "aqui é o corpo da função"
+#     return "esse será o retorno da função"
+
 def basic_sum(first_number, second_number):
     return
 
