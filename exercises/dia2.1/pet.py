9b2e856492883791073b46f6ae9a057175cc2e47
fkamag <fabiokamagian@gmail.com>
2023-02-04 16:43:28 -0300

    exercicios do dia

---
+++ exercises/dia2.1/pet.py
@@ -0,0 +1,18 @@
+class Pet():
+  def __init__(self, name, specie, age, human):
+    self.name = name
+    self.specie = specie
+    self.age = age
+    self.human = human
+
+  def __str__(self):
+    return f"""
+    - name: {self.name}
+    - specie: {self.specie}
+    - age: {self.age}
+    - human: {self.human}
+    """
+
+billy = Pet('Billy', 'Dog', 9, 'Fabio')
+
+print(billy)
