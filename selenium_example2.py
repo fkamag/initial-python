0a566d61864aad3543e785ab4520456eead273b1
fkamag <fabiokamagian@gmail.com>
2023-02-07 18:16:15 -0300

    input na busca

--- selenium_example2.py
+++ selenium_example2.py
@@ -1,5 +1,6 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 firefox = webdriver.Firefox()
 
@@ -9,6 +10,13 @@ options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--start-maximized')
 
 response = firefox.get('https://www.google.com/')
+
+search_input = firefox.find_element('name', 'q')
+search_input.send_keys('mister boni')
+
+sleep(2)
+
+search_input.send_keys(Keys.ENTER)
 
 sleep(5)
 firefox.quit()
