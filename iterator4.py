7a94c8ba7d5ecb530144672ce296846def8fc698
fkamag <fabiokamagian@gmail.com>
2023-02-19 23:07:33 -0300

    padrão iterator

---
+++ iterator4.py
@@ -0,0 +1,13 @@
+from iterator2 import DatabaseIterable
+from iterator1 import DbSimulator
+
+
+# Primeiro instanciamos o ITERÁVEL
+record_paginator = DatabaseIterable(DbSimulator(), "select * from person")
+
+# Em seguida podemos usar o for para iterar
+# Nesse momento o ITERADOR é criado implicitamente
+for page in record_paginator:
+    # faz algo com a página, que é uma lista de resultados
+    for record in page:
+        print(record["name"])
